eedf6f0efe169cc3804a87920a991de781143d58
Danoigun44 <danoig44@gmail.com>
2024-10-16 19:34:53 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,20 +1,16 @@
 
 variable "subscription_id"{
   type=string
-  default= "var.ecf831bb-9d6c-410f-ba46-1a620639498b"
 }
 
 variable "client_id"{
   type=string
-  default= "var.cb593eaa-b094-4dbf-b52e-316a5aa247a7"
 }
 variable "client_secret"{
   type=string
-  default= "var.DrT8Q~8y34-Qi5txMQOcMd_HQTZ1bSee4DhYKaDG"
 }
 variable "tenant_id"{
   type=string
-  default= "var.0ebdf2d5-6a77-45d3-b758-a500afb0460d"
 }
 variable "countNumber"{
   type=number
