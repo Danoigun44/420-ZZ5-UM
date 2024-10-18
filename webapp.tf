714e9c2493fd35b1d4e23db70356ac53a85bbe4e
Danoigun44 <danoig44@gmail.com>
2024-10-18 18:23:38 -0400

    Update webapp.tf

--- webapp.tf
+++ webapp.tf
@@ -2,21 +2,21 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+resource "azurerm_resource_group" "RG-420-ZZZ-UM" {
+  name     = "420-ZZZ-UM-resources"
+  location = "Canadacentral"
 }
 
-resource "azurerm_service_plan" "example" {
-  name                = "example"
+resource "azurerm_service_plan" "service-plan" {
+  name                = "420-ZZZ-UM-service-plan"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   sku_name            = "P1v2"
   os_type             = "Windows"
 }
 
-resource "azurerm_windows_web_app" "example" {
-  name                = "example"
+resource "azurerm_windows_web_app" "webapp1" {
+  name                = "webapp1"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_service_plan.example.location
   service_plan_id     = azurerm_service_plan.example.id
