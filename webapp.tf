9db73f0d87f9e19834a060482532f4cae1a20abf
Danoigun44 <danoig44@gmail.com>
2024-10-18 18:24:32 -0400

    Update webapp.tf

--- webapp.tf
+++ webapp.tf
@@ -23,3 +23,11 @@ resource "azurerm_windows_web_app" "webapp1" {
 
   site_config {}
 }
+resource "azurerm_windows_web_app" "webapp2" {
+  name                = "webapp2"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_service_plan.example.location
+  service_plan_id     = azurerm_service_plan.example.id
+
+  site_config {}
+}
