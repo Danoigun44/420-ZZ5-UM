9f411af73d16e39224010a5a4b6a844bd8e1c2d8
Danoigun44 <danoig44@gmail.com>
2024-10-18 20:36:31 -0400

    Update webapp2.tf

--- webapp2.tf
+++ webapp2.tf
@@ -1,31 +1,4 @@
  
-  webapp_names=["k8webapp06","k9webapp06","k10webapp06","k11webapp06","k12webapp06"]
-
-resource "azurerm_resource_group" "rg-az-group" {
-  name     = "rg_az_group"
-  location = "CanadaCentral"
-}
-
-resource "azurerm_webapp_cluster" "batchabcde" {
-  for_each            = {for cluster in var.classworkclusters: cluster=>cluster}
-  name                = "${var.prefix}${each.key}"
-  location            = azurerm_resource_group.rg-az-group.location
-  resource_group_name = azurerm_resource_group.rg-az-group.name
-  dns_prefix          = var.dnsprefix
-  service_plan_id = azurerm_service_plan.plan_service.id
-}
-
- 
-resource "azurerm_service_plan" "plan_service" {
- for_each            = {for cluster in var.classworkclusters: cluster=>cluster}
- name                = "${var.prefix}${each.key}"
-  location            = azurerm_resource_group.rg-az-group.location
-  resource_group_name = azurerm_resource_group.rg-az-group.name
-    sku_name            = "P1v2"
-  os_type             = "Windows"
-site_config {}
-}
-
 
 #This is an Azure Montreal College Tutorial for Storage Account creation--->Storage Container name Creation--->Storage Blob Creation
 locals{ 
