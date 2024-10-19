a2d7102d18a170cd3c6856ccccd1769125900bec
Danoigun44 <danoig44@gmail.com>
2024-10-18 20:08:24 -0400

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,13 +1,13 @@
 #This is an Azure Montreal College Tutorial for Storage Account creation--->Storage Container name Creation--->Storage Blob Creation
 locals{ 
-  cluster_names=["k1batcha06","k2batcha06","k3batcha06","k4batcha06","k5batcha06"]
+  webapp_names=["k1webapp06","k2webapp06","k3webapp06","k4webapp06","k5webapp06"]
 }
 resource "azurerm_resource_group" "rg-azgroup" {
   name     = "az_resource_group"
   location = "Canadacentral"
 }
-resource "azurerm_kubernetes_cluster" "batchabcd" {
-  for_each            = {for cluster in local.cluster_names: cluster=>cluster}
+resource "azurerm_kubernetes_webapp" "batchabcd" {
+  for_each            = {for webapp in local.webapp_names: cluster=>cluster}
   name                = "${var.prefix}cluster"
   location            = azurerm_resource_group.rg-azgroup.location
   resource_group_name = azurerm_resource_group.rg-azgroup.name
@@ -29,19 +29,19 @@ resource "azurerm_kubernetes_cluster" "batchabcd" {
 }
 /*
 output "client_certificate" {
-  value     = [for cluster in azurerm_kubernetes_cluster.batchabcd:cluster.kube_config.0.client_certificate]
+  value     = [for webapp in azurerm_kubernetes_webapp.batchabcd:webapp.kube_config.0.client_certificate]
   sensitive = true
 }
 
 output "kube_config" {
-  value = [for cluster in azurerm_kubernetes_cluster.batchabcd: cluster.kube_config_raw]
+  value = [for webapp in azurerm_kubernetes_webapp.batchabcd: webapp.kube_config_raw]
 
   sensitive = true
 }
 output "kube_id"{
-  value=[for cluster in azurerm_kubernetes_cluster.batchabcd:cluster.id ]
+  value=[for webapp in azurerm_kubernetes_webapp.batchabcd:webapp.id ]
 }
 output "kube_name"{
-  value=[for cluster in azurerm_kubernetes_cluster.batchabcd:cluster.name ]
+  value=[for webapp in azurerm_kubernetes_webapp.batchabcd:webapp.name ]
 }
 */
