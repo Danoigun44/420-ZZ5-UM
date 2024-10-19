72c55b81657163aee846d7b6bbc68e2f05b15c7a
Danoigun44 <danoig44@gmail.com>
2024-10-18 20:41:31 -0400

    Update webapp2.tf

--- webapp2.tf
+++ webapp2.tf
@@ -2,20 +2,20 @@
 
 #This is an Azure Montreal College Tutorial for Storage Account creation--->Storage Container name Creation--->Storage Blob Creation
 locals{ 
-  cluster_names=["k8webapp06","k9webapp06","k10webapp06","k11webapp06","k12webapp06"]
+  webapp2_names=["k8webapp06","k9webapp06","k10webapp06","k11webapp06","k12webapp06"]
 }
-resource "azurerm_resource_group" "rg-az-group" {
-  name     = "rg_az_group"
+resource "azurerm_resource_group" "reg-az-group" {
+  name     = "reg_az_group"
   location = "Canada Central"
 }
 
 resource "azurerm_service_plan" "planabcd" {
   for_each            = {for plan in var.classworkclusters: cluster=>cluster}
   name                = "${var.prefix}${each.key}"
-  location            = azurerm_resource_group.rg-az-group.location
-  resource_group_name = azurerm_resource_group.rg-az-group.name
+  location            = azurerm_resource_group.reg-az-group.location
+  resource_group_name = azurerm_resource_group.reg-az-group.name
   dns_prefix          = var.dnsprefix
-
+}
   default_node_pool {
     name       = "default"
     node_count = 1
