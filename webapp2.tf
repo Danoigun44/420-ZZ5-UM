1bcac93bd0eedac7caa7d601eaa719e7e01c71ed
Danoigun44 <danoig44@gmail.com>
2024-10-21 17:17:25 -0400

    Update webapp2.tf

--- webapp2.tf
+++ webapp2.tf
@@ -16,17 +16,3 @@ resource "azurerm_service_plan" "planabcd" {
   resource_group_name = azurerm_resource_group.reg-az-group.name
   dns_prefix          = var.dnsprefix
 }
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_D2_v2"
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  tags = {
-    Environment = "Production"
-  }
-
