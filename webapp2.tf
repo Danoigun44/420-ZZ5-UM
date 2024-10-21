2940d8f8108505a5c568b94bd934a1ea6388e14e
Danoigun44 <danoig44@gmail.com>
2024-10-21 19:28:20 -0400

    Update webapp2.tf

--- webapp2.tf
+++ webapp2.tf
@@ -10,7 +10,7 @@ resource "azurerm_resource_group" "reg-az-group" {
 }
 
 resource "azurerm_service_plan" "planabcd" {
-  for_each            = {for plan in var.classworkclusters: cluster=>cluster}
+  for_each            = {for plan in locals.webapp2_names: cluster=>cluster}
   name                = "${var.prefix}${each.key}"
   location            = azurerm_resource_group.reg-az-group.location
   resource_group_name = azurerm_resource_group.reg-az-group.name
