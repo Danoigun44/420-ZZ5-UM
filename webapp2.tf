85685d0c71965982639a675ef011675597eae788
Danoigun44 <danoig44@gmail.com>
2024-10-21 19:41:20 -0400

    Update webapp2.tf

--- webapp2.tf
+++ webapp2.tf
@@ -5,12 +5,12 @@ locals{
   webapp2_names=["k8webapp06","k9webapp06","k10webapp06","k11webapp06","k12webapp06"]
 }
 resource "azurerm_resource_group" "reg-az-group" {
-  name     = "reg_az_group"
+  name     = "regazgroup"
   location = "Canadacentral"
 }
 
 resource "azurerm_service_plan" "planabcd" {
-  for_each            = {for plan in locals.webapp2_names: cluster=>cluster}
+  for_each            = {for plan in local.webapp2_names: cluster=>cluster}
   name                = "${var.prefix}${each.key}"
   location            = azurerm_resource_group.reg-az-group.location
   resource_group_name = azurerm_resource_group.reg-az-group.name
