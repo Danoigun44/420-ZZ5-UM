8dd8e7c9e4884ca3d87507e0f7352af4a563c46d
Danoigun44 <danoig44@gmail.com>
2024-10-21 18:52:44 -0400

    Update module_storage_account.tf

--- module_storage_account.tf
+++ module_storage_account.tf
@@ -1,8 +1,1 @@
-module "storage_account" {
-  source = "./module_storage_account/"
 
-  storage_account_name  = local.storage_account_name
-  resource_group_name   = local.resource_group_name
-  location              = local.location
-  account_tier          = local.account_tier
-}
