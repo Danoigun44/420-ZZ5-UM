0ed87be8475ce36f4c6ecac9ebe25bfca1735d05
Danoigun44 <danoig44@gmail.com>
2024-10-21 20:21:09 -0400

    Update app_service_plan.tf

--- app_service_plan.tf
+++ app_service_plan.tf
@@ -1,8 +1,8 @@
 
 resource "azurerm_app_service_plan" "webapp2plan" {
   name                = "api-appserviceplan-pro"
-  location            = azurerm_resource_group.webapp2.location
-  resource_group_name = azurerm_resource_group.webapp2.name
+  location            = azurerm_resource_group.regazgroup.location
+  resource_group_name = azurerm_resource_group.regazgroup.name
 
   sku {
     tier = "Standard"
