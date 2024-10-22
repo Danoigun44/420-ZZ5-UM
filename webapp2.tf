6ba8146579f77828ff663e282fee92450434335b
Danoigun44 <danoig44@gmail.com>
2024-10-21 20:05:35 -0400

    Update webapp2.tf

--- webapp2.tf
+++ webapp2.tf
@@ -10,7 +10,7 @@ resource "azurerm_resource_group" "regazgroup" {
 }
 
 resource "azurerm_kubernetes_cluster" "planabcd" {
-  for_each            = {for plan in local.webapp2_names: cluster=>cluster}
+  for_each            = {for plan in local.webapp2_names: plan=>plan}
   name                = "${var.prefix}${each.key}"
   location            = azurerm_resource_group.regazgroup.location
   resource_group_name = azurerm_resource_group.regazgroup.name
