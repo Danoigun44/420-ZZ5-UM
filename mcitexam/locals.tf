63e4ba60786d738361bc5344b6b81784f80c4e11
Danoigun44 <danoig44@gmail.com>
2024-10-23 18:55:21 -0400

    Update locals.tf

--- mcitexam/locals.tf
+++ mcitexam/locals.tf
@@ -1,3 +1,11 @@
 # Define the names of the VMs and WAF policies
 locals {
   vms        = ["mcitexamvm1", "mcitexamvm2", "mcitexamvm3", "mcitexamvm4", "mcitexamvm5"]
+  waf_policies = {
+    "mcitwaf1" = "mcitexamvm1"
+    "mcitwaf2" = "mcitexamvm2"
+    "mcitwaf3" = "mcitexamvm3"
+    "mcitwaf4" = "mcitexamvm4"
+    "mcitwaf5" = "mcitexamvm5"
+  }
+}
