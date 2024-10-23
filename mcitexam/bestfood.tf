f3a9df1184822f185921c24dd50047c86984e5b1
Danoigun44 <danoig44@gmail.com>
2024-10-23 19:34:31 -0400

    Update bestfood.tf

--- mcitexam/bestfood.tf
+++ mcitexam/bestfood.tf
@@ -1,10 +1,16 @@
-# Define a list of the top 5 best foods
+# Define a map of the top 5 best foods
 locals {
-  best_foods = ["Rice", "Beans", "Chicken", "Beef", "Stew"]
+  best_foods = {
+    "food1" = "Rice"
+    "food2" = "Beans"
+    "food3" = "Chicken"
+    "food4" = "Beef"
+    "food5" = "Stew"
+  }
 }
 
-# Output the list of top 5 best foods
+# Output each food item using a for_each loop
 output "best_foods" {
   description = "The top 5 best foods"
-  value       = local.best_foods
+  value       = { for key, value in local.best_foods : key => value }
 }
