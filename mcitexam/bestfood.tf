df8a58740670b1ddda922fe23cc5c3d879c0d0e4
Danoigun44 <danoig44@gmail.com>
2024-10-23 20:10:39 -0400

    Update bestfood.tf

--- mcitexam/bestfood.tf
+++ mcitexam/bestfood.tf
@@ -1,16 +1,1 @@
-# Define a map of the top 5 best foods
-locals {
-  best_foods = {
-    "food1" = "Rice"
-    "food2" = "Beans"
-    "food3" = "Chicken"
-    "food4" = "Beef"
-    "food5" = "Stew"
-  }
-}
 
-# Output each food item using a for_each loop
-output "best_foods" {
-  description = "The top 5 best foods"
-  value       = { for key, value in local.best_foods : key => value }
-}
