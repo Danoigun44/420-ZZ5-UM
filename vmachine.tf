db403d5a30599e81418bb59d30e1b98053768c60
Danoigun44 <danoig44@gmail.com>
2024-10-25 18:57:56 -0400

    Update vmachine.tf

--- vmachine.tf
+++ vmachine.tf
@@ -1,51 +1,51 @@
 # Create a resource group
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "mcit420zz5um" {
   name     = var.resource_group_name
   location = var.location
 }
 
 # Create a virtual network
-resource "azurerm_virtual_network" "example_vnet" {
-  name                = "example-vnet"
+resource "azurerm_virtual_network" "mcit420zz5um_vnet" {
+  name                = "mcit420zz5um-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.mcit420zz5um.location
+  resource_group_name = azurerm_resource_group.mcit420zz5um.name
 }
 
 # Create a subnet
-resource "azurerm_subnet" "example_subnet" {
-  name                 = "example-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example_vnet.name
+resource "azurerm_subnet" "mcit420zz5um_subnet" {
+  name                 = "mcit420zz5um-subnet"
+  resource_group_name  = azurerm_resource_group.mcit420zz5um.name
+  virtual_network_name = azurerm_virtual_network.mcit420zz5um_vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 # Create network interfaces for each VM
-resource "azurerm_network_interface" "example_nic" {
+resource "azurerm_network_interface" "mcit420zz5um_nic" {
   for_each            = var.vm_names
   name                = "${each.value}-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.mcit420zz5um.location
+  resource_group_name = azurerm_resource_group.mcit420zz5um.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.example_subnet.id
+    subnet_id                     = azurerm_subnet.mcit420zz5um_subnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 # Create VMs using for_each
-resource "azurerm_linux_virtual_machine" "example_vm" {
+resource "azurerm_linux_virtual_machine" "mcit420zz5um_vm" {
   for_each            = var.vm_names
   name                = each.value
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.mcit420zz5um.name
+  location            = azurerm_resource_group.mcit420zz5um.location
   size                = "Standard_B1ms"
 
   admin_username      = "adminuser"
 
   network_interface_ids = [
-    azurerm_network_interface.example_nic[each.key].id
+    azurerm_network_interface.mcit420zz5um_nic[each.key].id
   ]
 
   admin_ssh_key {
