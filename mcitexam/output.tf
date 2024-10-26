e387a4353366390006b19b5010472913e2005db4
Danoigun44 <danoig44@gmail.com>
2024-10-25 20:03:45 -0400

    Update output.tf

--- mcitexam/output.tf
+++ mcitexam/output.tf
@@ -1,12 +1,12 @@
 # Output for VMs
 output "vm_ids" {
   description = "The IDs of the VMs"
-  value       = { for vm in azurerm_linux_virtual_machine.regazgroup_vm : vm.name => vm.id }
+  value       = { for vm in azurerm_linux_virtual_machine.mcit420zz5um_vm : vm.name => vm.id }
 }
 
 # Output for WAF Policies
 output "waf_policy_ids" {
   description = "The IDs of the WAF policies"
-  value       = { for waf in azurerm_web_application_firewall_policy.regazgroup_waf : waf.name => waf.id }
+  value       = { for waf in azurerm_web_application_firewall_policy.mcit420zz5um_waf : waf.name => waf.id }
 }
 
